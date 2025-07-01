1de8073d057a190f9760451a1c7ac610ffefb64b
Jakob Karlsmoen <jakob.karlsmoen@soprasteria.com>
2025-07-01 12:44:54 +0200

    Add tiny-code-action plugin

---
+++ lua/plugins/tiny-code-action.lua
@@ -0,0 +1,19 @@
+return {
+	"rachartier/tiny-code-action.nvim",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ "nvim-telescope/telescope.nvim" },
+	},
+	event = "LspAttach",
+	opts = {},
+	keys = {
+		{
+			"<leader>ca",
+			function()
+				require("tiny-code-action").code_action()
+			end,
+			mode = "n",
+			desc = "Show code actions",
+		},
+	},
+}
