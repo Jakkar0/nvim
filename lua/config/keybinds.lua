ef165f7ba3026f6deba6e9565b7fbbd1d1f1eb3f
Jakob <jakkar@mailbox.org>
2025-08-26 21:49:08 +0200

    Add keymaps for copying to clipboard

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -9,3 +9,9 @@ vim.keymap.set("n", "<C-S-Up>", "<cmd>resize +2<cr>")
 vim.keymap.set("n", "<C-S-Down>", "<cmd>resize -2<cr>")
 vim.keymap.set("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>")
 vim.keymap.set("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>")
+
+-- copy to system clipboard by prefixing yang with leader
+vim.keymap.set({ "n", "x" }, "<leader>y", '"+y')
+vim.keymap.set({ "n", "x" }, "<leader>Y", '"+yg_')
+vim.keymap.set({ "n", "x" }, "<leader>p", '"+p')
+vim.keymap.set({ "n", "x" }, "<leader>P", '"+P')
